39c63ffa97e94568f5ccaad8d8c2aa174704ae5a
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-07-05 17:54:06 +1000

    testing LLMs

--- ai/llm.py
+++ ai/llm.py
@@ -8,10 +8,10 @@ import ollama
 
 def ask_llm(prompt):
     response = ollama.chat(
-        # model="llama3.1:8b",
+        model="llama3.1:8b",
         # model="qwen2.5-coder:7b",
         # model="qwen3.5:0.8b",
-        model="qwen3.5:9b",
+        # model="qwen3.5:9b",
         messages=[{"role": "user", "content":prompt}]
     )
     return response["message"]["content"]
